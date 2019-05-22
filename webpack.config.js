4fafc8da552aa54877daee7acc35239eb55ee8d0
taehyunlim <taehyun.lim@gmail.com>
2019-05-22 13:29:26 -0700

    Webpack dev tool

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,14 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
 
 module.exports = {
+  mode: 'development',
   entry: {
     app: './src/index.js',
     print: './src/print.js'
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist'
   },
   plugins: [
     new CleanWebpackPlugin(),
