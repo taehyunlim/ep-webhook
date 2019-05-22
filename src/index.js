e4502f7c5ba91a773a5b1515974573d96d7a60ed
taehyunlim <taehyun.lim@gmail.com>
2019-05-22 11:18:14 -0700

    Asset Mgmt: CSS and Images

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import _ from 'lodash'
+import './style.css'
+import Icon from './icon.png'
 
 function component() {
   const element = document.createElement('div');
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('test')
+
+  const iconImage = new Image()
+  iconImage.src = Icon;
+
+  element.appendChild(iconImage)
+
   return element;
 }
 
