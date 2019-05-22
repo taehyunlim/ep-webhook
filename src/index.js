0b7e66983642d0ffe16936b4d5dad73776b6d9dc
taehyunlim <taehyun.lim@gmail.com>
2019-05-22 11:41:12 -0700

    Output management

--- src/index.js
+++ src/index.js
@@ -1,18 +1,27 @@
 import _ from 'lodash'
 import './style.css'
 import Icon from './icon.png'
+import printMe from './print.js'
 
 function component() {
-  const element = document.createElement('div');
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  const element = document.createElement('div')
+  const btn = document.createElement('button')
+
+  // Bundle: Lodash
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ')
   element.classList.add('test')
 
+  // Asset Management: Image
   const iconImage = new Image()
-  iconImage.src = Icon;
-
+  iconImage.src = Icon
   element.appendChild(iconImage)
+
+  // Output Management
+  btn.innerHTML = 'Click and check console'
+  btn.onclick = printMe;
+  element.append(btn);
 
   return element;
 }
 
-document.body.appendChild(component());
+document.body.appendChild(component())
